31993c2eb6f2be78e919d53168647be07d863522
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-04-20 15:09:25 -0400

    attempt to fix kokoro ubuntu errors

---
+++ src/test/java/com/google/cloud/tools/maven/util/SocketUtil.java
@@ -0,0 +1,19 @@
+package com.google.cloud.tools.maven.util;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class SocketUtil {
+
+  /**
+   * Returns a port that's available.
+   * 
+   * <p><i>Note: the port may become unavailabe by the time the caller tries to use it.</i>
+   */
+  public static int findPort() throws IOException {
+    try (ServerSocket serverSocket = new ServerSocket(0)) {
+      serverSocket.setReuseAddress(true);
+      return serverSocket.getLocalPort();
+    }
+  }
+}
