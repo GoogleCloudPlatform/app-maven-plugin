ebb0f952855820fbeec8de4382b31f81f25fb5cb
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-05-12 12:20:51 -0400

    update macos build scripts to use install Maven for integration tests

--- kokoro/continuous_mac.sh
+++ kokoro/continuous_mac.sh
@@ -9,6 +9,14 @@ curl https://sdk.cloud.google.com | bash
 GOOGLE_CLOUD_SDK_HOME=/Users/kbuilder/google-cloud-sdk
 "$GOOGLE_CLOUD_SDK_HOME"/bin/gcloud components install app-engine-java
 
+# temporary workaround until mvn is available in the image by default
+# the integration tests rely on Maven being installed, cannot use the wrapper
+wget http://www-us.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.zip
+unzip apache-maven-3.5.0-bin.zip
+
+M2_HOME="$(pwd)"/apache-maven-3.5.0
+PATH=$PATH:$M2_HOME/bin
+
 cd github/app-maven-plugin
 ./mvnw clean install cobertura:cobertura -B -U
 # bash <(curl -s https://codecov.io/bash)
