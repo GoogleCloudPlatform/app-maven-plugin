93efe69491ccaaf2b64dfacaabac60a2e24abae2
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-05-12 12:32:46 -0400

    update macos build scripts to tar arguments

--- kokoro/continuous_mac.sh
+++ kokoro/continuous_mac.sh
@@ -12,7 +12,7 @@ GOOGLE_CLOUD_SDK_HOME=/Users/kbuilder/google-cloud-sdk
 # temporary workaround until mvn is available in the image by default
 # the integration tests rely on Maven being installed, cannot use the wrapper
 curl http://www-us.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz -o apache-maven-3.5.0-bin.tar.gz
-tar -xvz apache-maven-3.5.0-bin.tar.gz
+tar -xzf apache-maven-3.5.0-bin.tar.gz
 
 M2_HOME="$(pwd)"/apache-maven-3.5.0
 PATH=$PATH:$M2_HOME/bin
