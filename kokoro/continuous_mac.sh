a1ed7459c86c364f63867e492a0ed82b3d6a91e0
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-05-12 12:24:17 -0400

    update macos build scripts to use curl and tar instead of wget and unzip

--- kokoro/continuous_mac.sh
+++ kokoro/continuous_mac.sh
@@ -11,8 +11,8 @@ GOOGLE_CLOUD_SDK_HOME=/Users/kbuilder/google-cloud-sdk
 
 # temporary workaround until mvn is available in the image by default
 # the integration tests rely on Maven being installed, cannot use the wrapper
-wget http://www-us.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.zip
-unzip apache-maven-3.5.0-bin.zip
+curl http://www-us.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz -o apache-maven-3.5.0-bin.tar.gz
+tar -xvz apache-maven-3.5.0-bin.tar.gz
 
 M2_HOME="$(pwd)"/apache-maven-3.5.0
 PATH=$PATH:$M2_HOME/bin
