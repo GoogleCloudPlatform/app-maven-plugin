55cce6782a24e01317bedd79a21bbb5e96b996e9
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-05-12 14:13:44 -0400

    download maven from ct4j build cache

--- kokoro/continuous_mac.sh
+++ kokoro/continuous_mac.sh
@@ -11,7 +11,7 @@ GOOGLE_CLOUD_SDK_HOME=/Users/kbuilder/google-cloud-sdk
 
 # temporary workaround until mvn is available in the image by default
 # the integration tests rely on Maven being installed, cannot use the wrapper
-curl http://www-us.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz -o apache-maven-3.5.0-bin.tar.gz
+curl  https://storage.googleapis.com/cloud-tools-for-java-team-kokoro-build-cache/apache-maven-3.5.0-bin.tar.gz -o apache-maven-3.5.0-bin.tar.gz
 tar -xzf apache-maven-3.5.0-bin.tar.gz
 
 M2_HOME="$(pwd)"/apache-maven-3.5.0
